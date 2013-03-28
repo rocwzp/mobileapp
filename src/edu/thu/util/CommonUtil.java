d4371f7582df3a2d369459f017350c4c12a18f0d
sangesot <sangesot@163.com>
2013-03-28 10:50:32 +0800

    des-->design

--- src/edu/thu/util/CommonUtil.java
+++ src/edu/thu/util/CommonUtil.java
@@ -9,9 +9,9 @@ public class CommonUtil {
 	public static final String DES = "des";
 	public static final String JST = "jst";
 
-	public static final String JNDI_DES = "java:comp/env/des";
-	public static final String JNDI_RES = "java:comp/env/res";
-	public static final String JNDI_JST = "java:comp/env/jst";
+	public static final String JNDI_DES = "java:comp/env/design";
+	public static final String JNDI_RES = "java:comp/env/resources";
+	public static final String JNDI_JST = "java:comp/env/jst_study";
 	public static final String JNDI_PORTAL = "java:comp/env/portal";
 
 	public static final int RESULT_CODE_DEFAULT = -1;
